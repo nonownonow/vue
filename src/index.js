45d79cca055c847a1e37770f70d86f53aff48168
ilhwan.ryu <ih.ryu.dev@gmail.com>
2019-08-20 11:15:29 +0900

    setting the dev-server and postcss

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import { join } from 'fxjs2'
+import print from './print'
+function component () {
+  const element = document.getElementById('app')
+  const button = document.createElement('button')
+  button.innerText = 'Click'
+  button.addEventListener('click', e => {
+    print()
+  })
+  element.innerText = join(' ', ['hello', 'webpack'])
+  element.appendChild(button)
+  return element
+}
+
+document.body.replaceWith(component())
---
+++ src/print.js
@@ -0,0 +1,3 @@
+export default function () {
+  consol.log('Nice to meet you!')
+}

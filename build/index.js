cbc1bda05be9400fe19abb8d1eac0d7c6955a7ee
ilhwan.ryu <ih.ryu.dev@gmail.com>
2019-08-20 12:22:32 +0900

    wip

---
+++ build/index.js
@@ -0,0 +1,13 @@
+// const path = require('path')
+const webpack = require('webpack')
+// const config = require('../config')
+// const webpackConfig = require('./webpack.prod.conf')
+
+console.log(process.env.NODE_ENV)
+webpack({
+
+}, (err, stats)=>{
+  if(err||status.hasErros()){
+
+  }
+})
